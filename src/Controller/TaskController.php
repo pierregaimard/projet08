746d76baf8d9d16f795bdfae7723902655c4cd06
Pierre Gaimard <pierre@gaimard-family.fr>
2021-03-07 21:26:39 +0100

    Change TaskType use statement

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use AppBundle\Entity\Task;
-use AppBundle\Form\TaskType;
+use App\Form\TaskType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
