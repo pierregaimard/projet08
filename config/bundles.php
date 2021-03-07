516eab3cdeaab5458280978348cf4a79b13df0b3
Pierre Gaimard <pierre@gaimard-family.fr>
2021-03-07 17:51:13 +0100

    Remove vendor dir and re-install project dependencies using flex

---
+++ config/bundles.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+];
