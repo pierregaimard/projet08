96ffededa5df82b76abe90e5044140ba423145b6
Pierre Gaimard <pierre@gaimard-family.fr>
2021-03-12 13:16:26 +0100

    Add data transformer to roles choiceType in UserType

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,11 +4,14 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
 {
@@ -32,5 +35,15 @@ class UserType extends AbstractType
                 'label' => 'Role',
             ])
         ;
+
+        $builder->get('roles')->addModelTransformer(new CallbackTransformer(
+            function ($roleAsArray) {
+                // transform the array to a string
+                return $roleAsArray[0];
+            },
+            function ($roleAsString) {
+                // transform the string back to an array
+                return [$roleAsString];
+            }
+        ));
     }
-}
