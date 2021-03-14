e96cfb590156839ae50052d15037b65e7b5ba080
Pierre Gaimard <pierre@gaimard-family.fr>
2021-03-14 17:57:33 +0100

    Improve testTaskAnonymousOwner method

--- tests/Entity/TaskTest.php
+++ tests/Entity/TaskTest.php
@@ -11,7 +11,14 @@ final class TaskTest extends TestCase
     public function testTaskAnonymousOwner()
     {
         $task = new Task();
+        # If no owner set Task returns an anonymous owner who is instanceof User
         $this->assertTrue($task->getOwner() instanceof User);
         $this->assertEquals(Task::OWNER_ANONYMOUS, $task->getOwner()->getUsername());
+
+        $user = new User();
+        $user->setUsername('TaskOwner');
+        $task->setOwner($user);
+        # If owner is set, this one is returned.
+        $this->assertEquals('TaskOwner', $task->getOwner()->getUsername());
     }
 }
