d2e1ab351d19184ba8222e2bcebbf4f9e7b53a05
Pierre Gaimard <pierre@gaimard-family.fr>
2021-03-16 21:05:41 +0100

    Add UserTest test class

---
+++ tests/Entity/UserTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\User;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    public function testUserRole()
+    {
+        $user = new User();
+
+        # Initial user role should be ROLE_USER
+        $this->assertEquals([User::ROLE_USER], $user->getRoles());
+        $this->assertEquals('Utilisateur', $user->getRole());
+
+        $user->setRoles([User::ROLE_ADMIN]);
+
+        # Test with ROLE_ADMIN
+        $this->assertEquals([User::ROLE_ADMIN], $user->getRoles());
+        $this->assertEquals('Administrateur', $user->getRole());
+    }
+}
