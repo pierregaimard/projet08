68eb8f88b9a8feca62a5833248d8dd5b5d44e605
Pierre Gaimard <pierre@gaimard-family.fr>
2021-03-26 14:38:17 +0100

    Improve testUpgradePassword test method

--- tests/Repository/UserRepositoryTest.php
+++ tests/Repository/UserRepositoryTest.php
@@ -2,9 +2,12 @@
 
 namespace App\Tests\Repository;
 
+use App\Entity\Task;
 use App\Entity\User;
 use App\Test\AbstractAppWebTestCase;
 use Hautelook\AliceBundle\PhpUnit\ReloadDatabaseTrait;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 final class UserRepositoryTest extends AbstractAppWebTestCase
 {
@@ -20,5 +23,10 @@ final class UserRepositoryTest extends AbstractAppWebTestCase
 
         $user = $userRepository->findOneBy(['username' => 'TestUser']);
         $this->assertEquals('MyNewEncodedPassword', $user->getPassword());
+
+        $mockUserInterface = $this->createMock(UserInterface::class);
+
+        $this->expectException(UnsupportedUserException::class);
+        $userRepository->upgradePassword($mockUserInterface, 'FakePassword');
     }
 }
